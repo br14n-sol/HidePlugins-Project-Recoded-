cd8a54a6e076af64268abe5af7156e4795f8c150
br14n-sol <60621046+br14n-sol@users.noreply.github.com>
2022-07-05 12:52:41 -0500

    Initial commit

---
+++ src/main/java/org/br14n_sol/HidePlugins_Project.java
@@ -0,0 +1,15 @@
+package org.br14n_sol;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HidePlugins_Project extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
